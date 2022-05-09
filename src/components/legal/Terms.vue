310cb0351bb07f93efa809e30a923a06a41e5fe9
Martin Kruusement <martinkruusement@gmail.com>
2022-05-09 18:16:37 +0300

    fix Terms page

--- src/components/legal/Terms.vue
+++ src/components/legal/Terms.vue
@@ -2,8 +2,7 @@
   <div class="page-content">
     <div class="page-head">
       <div class="page-title">
-        <h2>{{ $t('transactions.title') }}</h2>
-        <h4>{{ $t('transactions.caption', [transactionId]) }}</h4>
+        <h2>{{ $t('terms.title') }}</h2>
       </div>
     </div>
     <div class="page-body">
@@ -18,30 +17,6 @@ export default {
   name: 'Terms',
   components: {
     TermsContent
-  },
-  props: {
-    transactionId: String
-  },
-  computed: {
-    transaction () {
-      return this.$store.state.transactions.transactions[this.transactionId]
-    },
-    loading () {
-      // TODO: implement individual transaction loading
-      return this.$store.state.transactions.loading[this.transactionId]
-    },
-    gateway () {
-      if (this.transaction) {
-        return this.$store.getters.gateway(this.transaction.gatewayId)
-      }
-      return null
-    },
-    project () {
-      if (this.transaction) {
-        return this.$store.getters.project(this.transaction.projectId)
-      }
-      return null
-    }
   }
 }
 </script>
