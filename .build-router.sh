f37959c5f4984e82ac56582b8018ac579ca97805
Martin Kruusement <martinkruusement@gmail.com>
2022-05-22 02:58:34 +0300

    auto-fix package.json on save

--- .build-router.sh
+++ .build-router.sh
@@ -3,11 +3,6 @@
 # TODO: See if we can match any $2.build_router[filenameRegex]: "command"
 # TODO: If yes, run that command to deal with the file
 
-echo $0
-echo $1
-echo $2
-echo $3
-
 if [[ $1 == "package.json" ]]
 then
 npm run lint:package
