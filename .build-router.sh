f01a46d42d4fe66f6f29be74f097f930dc21eac4
Martin Kruusement <martinkruusement@gmail.com>
2022-05-22 04:29:10 +0300

    pattern-matching build-routing black magic

--- .build-router.sh
+++ .build-router.sh
@@ -1,10 +1,24 @@
 # $1: File name
 # $2 .sublime-project file name
-# TODO: See if we can match any $2.build_router[filenameRegex]: "command"
-# TODO: If yes, run that command to deal with the file
 
-if [[ $1 == "package.json" ]]
-then
-npm run lint:package
-fi  
+# Parse sublime-project.build_router and glob-match "pattern: command":
+# {
+#  "package-*.json": "npm run lint:package"
+#  "*.css": "echo Nice got your css file",
+#  "user.yml": "ls -hal",
+# }
 
+file=\"$1\"
+project_file=$2
+
+jq ".build_router | to_entries[] | .key, .value" $project_file |
+while IFS= read -r key && IFS= read -r value; do
+  if echo $file | grep -e "$key" > /dev/null; then
+    echo $file matches $key
+    echo "yes match"
+    echo "$file: [$key] routed to command [$value]"
+    cmd="${value%\"}"
+    cmd="${cmd#\"}"
+    eval $cmd
+  fi
+done
